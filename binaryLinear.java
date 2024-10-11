23a4d24cf44ffc96d8ee666c3bb59d8893800db5
Jamie <jamie.mcmenemy.2025@mumail.ie>
2024-10-11 11:35:14 +0100

    Finished learning binary and linear search.

--- binaryLinear.java
+++ binaryLinear.java
@@ -1,13 +1,13 @@
-public class Demo {
+public class binaryLinear {
     public static void main(String[] args) {
         int nums[] = new int[1000];
         int target = 18;
 
         
-        int result2 = binarySearch(nums, target, 0, nums.length-1);
+        int result = binarySearch(nums, target, 0, nums.length-1);
 
-        if (result2 != -1){
-            System.out.println("The index is: " + result2);
+        if (result != -1){
+            System.out.println("The index is: " + result);
         }
         
     }
@@ -16,16 +16,13 @@ public class Demo {
         while (startPoint <= endPoint) {
             
             int midPoint = (startPoint + endPoint)/2;
-            if (nums[midPoint] == target) {
+            if (nums[midPoint] == target) 
                 return midPoint;
-            }
             else if (target < nums[midPoint])
-            {
+            
                 return binarySearch(nums, target, startPoint, midPoint -1);
-            }
-            else {
+            else
                 return binarySearch(nums, target, midPoint+1, endPoint);
-            }
         }
         return -1;
     }
