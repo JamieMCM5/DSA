f14ba659441ec0c360e74e4f784ae28ff05292f6
Jamie <mcmenemyjamie@gmail.com>
2024-10-19 20:38:41 +0100

    Update insertSortAndSearch.java

--- insertSortAndSearch.java
+++ insertSortAndSearch.java
@@ -14,15 +14,24 @@ public class insertSortAndSearch {
     }
 
     public static int[] insertionSort(int[] arr) {
+
+        //Declare standard variables
         int n = arr.length;
+
+        //Iterate over arr, starting from index 1, set the key to the current value, and
+        //set j to the index behind the current index
         for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i-1;
 
+            //While the value of index j is greater than the key
             while (j>=0 && arr[j] > key) {
+                
+                //Shift the value of arr[j] to the next index
                 arr[j+1] = arr[j];
                 j--;
             }
+            //Make the key be the value of the index j+1;
             arr[j+1] = key;
         }
         return arr;
